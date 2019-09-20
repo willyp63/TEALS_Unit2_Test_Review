5a21b36511ac3ea1ed85699e63ce89e773a78d29
Pirino, Wil <wil.pirino@transamerica.com>
2019-09-20 13:36:56 -0600

    better code comments

--- solutions/FlippedStairs1.java
+++ solutions/FlippedStairs1.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    for (int y = 10; y > 0; y--) {
-      for (int x = 0; x < y; x++) {
+    // reverse the direction of our loop
+    // instead of going from 0 to 9, we now go from 9 to 0
+    for (int y = 10 - 1; y >= 0; y--) {
+      for (int x = 0; x <= y; x++) {
         System.out.print("X");
       }
       System.out.println();
@@ -9,6 +11,7 @@ public class Main {
 
     // OR
 
+    // same thing except we are doing the reversing in our inner loop by replacing y with (10 - y)
     for (int y = 0; y < 10; y++) {
       for (int x = 0; x < (10 - y); x++) {
         System.out.print("X");
--- solutions/StretchedStairs1.java
+++ solutions/StretchedStairs1.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     for (int y = 1; y <= 10; y++) {
+      // triple the number of iterations by replacing y with (y * 3)
       for (int x = 0; x < (y * 3); x++) {
         System.out.print("X");
       }
